c7a003da121b569d4e59b53543f9985c2833c436
carlos sanchez <carlos.sanchez.a0@gmail.com>
2021-12-03 19:46:50 -0500

    maquetando login y usando un provider para controlar el correo y clave

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -1,20 +1,26 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mintic_app/providers/auth_login_provider.dart';
+import 'package:provider/provider.dart';
 
-class LoginPage extends StatelessWidget{
-
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _media = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Volver'),
-      ),
       body: Container(
-        child: Column(
-          children: [
-            Text('hola mundo'),
-
-            _Formulario()
-          ],
+        //color: Colors.red[200],
+        width: double.infinity ,
+        height: _media.height,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ChangeNotifierProvider(
+                create: (_) => AuthLoginProvider(),
+                child: _Formulario(),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -22,17 +28,57 @@ class LoginPage extends StatelessWidget{
 }
 
 class _Formulario extends StatelessWidget {
-  const _Formulario({
-    Key key,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        children: [
-          Text('Inicio de sesion')
-        ],
+    final _formProvider = Provider.of<AuthLoginProvider>(context);
+    final _media = MediaQuery.of(context).size;
+    return Container(
+      // color: Colors.blue[300],
+      height: _media.height,
+      padding: EdgeInsets.all(8),
+      child: Form(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Inicio de sesion',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            SizedBox(height: 15),
+            TextFormField(
+              onChanged: (value) => _formProvider.correo = value,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Correo electronico',
+                hintText: 'Ej. correo@electronico.com',
+              ),
+            ),
+            SizedBox(height: 15),
+            TextFormField(
+              onChanged: (value) => _formProvider.clave = value,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Clave',
+                hintText: '******',
+              ),
+            ),
+            SizedBox(height: 15),
+            Material(
+              child: MaterialButton(
+                minWidth: double.infinity,
+                color: Theme.of(context).primaryColor,
+                onPressed: () {
+                  print(_formProvider.correo);
+                  print(_formProvider.clave);
+                },
+                child: Text(
+                  'Ingresar',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/providers/auth_login_provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AuthLoginProvider extends ChangeNotifier{
+  String? correo;
+  String? clave;
+  bool _isValid = false;
+
+
+}
